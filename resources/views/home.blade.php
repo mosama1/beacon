b496e993658b11ff0e16a0390fba87d254943535
mosama1 <jm_16_2008@hotmail.com>
2017-01-24 10:57:47 -0400

    validacion de 5 digitos ydos cedimales para agregar menu (COUPONS)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,8 +34,8 @@
             <img src="img/icons/promociones.png" title="Promociones">
           </a>
           <ul>
-            <li><a href=""><span>Kit de <br>Bienvenida</span></a></li>
-            <li><a href=""><span>Kit de <br>Fidelidad</span></a></li>
+            <li><a href="#kitBienvenida"><span>Kit de <br>Bienvenida</span></a></li>
+            <li><a href="#kitFidelidad"><span>Kit de <br>Fidelidad</span></a></li>
           </ul>
         </li>
       </ul>
