30371484a25d479572fa2e88a7e1ab907ab88f06
mosama1 <jm_16_2008@hotmail.com>
2017-01-26 12:17:40 -0400

    cambios

--- resources/views/layouts/appFinal.blade.php
+++ resources/views/layouts/appFinal.blade.php
@@ -158,6 +158,8 @@
     <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="js/materialize.js"></script>
     <script src="js/init.js"></script>
+    <script src="js/jquery.mask.min.js"></script>
+
     <script src="js/script.js"></script>
 
     @if (session('status'))
